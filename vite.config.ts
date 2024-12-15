688d2e1017f6cc58270296a48fcb98ab276e1cb3
StyleList94 <43318609+StyleList94@users.noreply.github.com>
2024-12-15 17:57:53 +0900

    feat: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-/// <reference types="vitest" />
+/// <reference types="vitest/config" />
 /// <reference types="vite/client" />
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,4 +13,4 @@ export default defineConfig({
     setupFiles: './src/config/vitest.setup.ts',
     css: true,
   },
-})
+});
